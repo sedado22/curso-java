1058049122bd0dce9960e5fd94866af17e3ad5c7
sedado22 <marcospousada@gmail.com>
2024-04-17 19:57:36 -0300

    fin clase 13

---
+++ CLASE13/script.js
@@ -0,0 +1,44 @@
+/** Organización del código:
+ * El código en JS se puede estructurar en las secciones generales:
+ * 1. Comentarios de la cabecera.
+ * 2. Importación de librerías externas.
+ * 3. Importación de módulos locales.
+ * 4. Definición de variables globales.
+ * 5. Declaración de funciones.
+ * 6. Ejecución principal del código, procesos.
+ * 7. Resultados. 
+ */
+
+
+// organizacion del codigo
+
+// Declaracion de variables e inicializacion tipo: let, var, const
+let num1; 
+// Inicializacion o asignacion de la variable
+num1 = 10;
+num1 = 20.5;
+num1 = "hola mundo";
+
+// Instrucciones de salida
+// 1- Salida de informacion a traves de la consola
+console.log(var1);
+
+// 2 - Utilizando alert
+//alert(num1);
+
+// 3- Utilizando document.wite()
+document.write(var1);
+
+// Variable tipo var
+// Declaracion e inicializacion de variable tipo var
+
+var var1 = 20;
+var var1 = 30; //redeclaracion de la variable
+
+//Variables tipo const
+
+const PI = 3.14; //no permite reasignar valor a una variable
+
+
+
+
